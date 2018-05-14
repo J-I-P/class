3b314e0fae967bf561ce39af961b0e9eddc5e328
jypin <apple80177@gmail.com>
2018-05-14 13:44:49 +0800

    new line3

--- ch04_ospath.py
+++ ch04_ospath.py
@@ -4,7 +4,7 @@ print("目前路徑：" + cur_path)
 filename = ospath.abspath("ch04_ospath.py")
 if ospath.exists(filename):
     print("完整路徑名稱：" + filename)
-    print("檔案大小：" + ospath.getsize(filename))
+    print("檔案大小：" + str(ospath.getsize(filename)))
 
     basename = ospath.basename(filename)
     print("最後的檔案或路徑名稱：" + basename)
@@ -12,9 +12,15 @@ if ospath.exists(filename):
     dirname = ospath.dirname(filename)
     print("目前檔案目錄路徑：" + dirname)
 
-    print("是否為目錄：" + ospath.isdir(filename))
+    print("是否為目錄：" + str(ospath.isdir(filename)))
 
-    fullname, fname = ospath.split(filename)
-    print("目錄名稱：" + fullname)
+    fullpath, fname = ospath.split(filename)
+    print("目錄名稱：" + fullpath)
     print("檔名：" + fname)
 
+    Drive, fpath = ospath.splitdrive(filename)
+    print("磁碟機：" + Drive)
+    print("路徑名稱" + fpath)
+
+    fulllpath = ospath.join(fullpath + "//" + fname)
+    print("組合路徑：" + fullpath)
